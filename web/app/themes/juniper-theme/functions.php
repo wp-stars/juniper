7672fc8636f203b7b8db46c61c58e3e93a23589b
Will Nahmens <willnahmens@gmail.com>
2023-12-14 16:00:03 +0100

    update styles for buttons and turn gf input into button

--- web/app/themes/juniper-theme/functions.php
+++ web/app/themes/juniper-theme/functions.php
@@ -292,3 +292,33 @@ function change_gravity_forms_language($locale) {
 
     return $locale;
 }
+
+
+/**
+* Filters the next, previous and submit buttons.
+* Replaces the form's <input> buttons with <button> while maintaining attributes from original <input>.
+*
+* @param string $button Contains the <input> tag to be filtered.
+* @param object $form Contains all the properties of the current form.
+*
+* @return string The filtered button.
+*/
+add_filter( 'gform_next_button', 'input_to_button', 10, 2 );
+add_filter( 'gform_previous_button', 'input_to_button', 10, 2 );
+add_filter( 'gform_submit_button', 'input_to_button', 10, 2 );
+function input_to_button( $button, $form ) {
+     
+    $dom = new DOMDocument();
+    $dom->loadHTML( '<?xml encoding="utf-8" ?>' . $button );
+    $input = $dom->getElementsByTagName( 'input' )->item(0);
+    $new_button = $dom->createElement( 'button' );
+    $new_button->appendChild( $dom->createTextNode( $input->getAttribute( 'value' ) ) );
+    $input->removeAttribute( 'value' );
+    foreach( $input->attributes as $attribute ) {
+        $new_button->setAttribute( $attribute->name, $attribute->value );
+    }
+    $new_button->setAttribute('class', 'btn btn-primary cursor-pointer');
+    $input->parentNode->replaceChild( $new_button, $input );
+ 
+    return $dom->saveHtml( $new_button );
+}

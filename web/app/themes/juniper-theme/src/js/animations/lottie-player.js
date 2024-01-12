6c6c40eb7c92362c96e91e175fa4070e83c81ec7
Will Nahmens <willnahmens@gmail.com>
2024-01-12 06:18:46 +0100

    update the lottie player

--- web/app/themes/juniper-theme/src/js/animations/lottie-player.js
+++ web/app/themes/juniper-theme/src/js/animations/lottie-player.js
@@ -23,3 +23,30 @@ targetBtns.forEach((btn, index) => {
       window[`linkBtn-${index}`].play();
    });
 })
+
+// select all 
+let targetLightBtns = document.querySelectorAll('.btn.btn-underline-white:not(.cardgrid-btn)')
+
+targetLightBtns.forEach((btn, index) => {
+   window[`linkBtnLight-${index}`] = bodymovin.loadAnimation({
+      // animationData: { /* ... */ },
+      container: btn, // required
+      path: lottieData.assetPath + '/json/white-link-animation.json', // required
+      renderer: 'svg', // required
+      loop: false, // optional
+      autoplay: false, // optional
+      name: "Link Animation", // optional
+   });
+
+   let directionMenu = 1;
+   btn.addEventListener('mouseenter', (e) => {
+      window[`linkBtnLight-${index}`].setDirection(directionMenu);
+      window[`linkBtn-${index}`].play();
+   });
+
+   btn.addEventListener('mouseleave', (e) => {
+      window[`linkBtnLight-${index}`].setDirection(-directionMenu);
+      window[`linkBtnLight-${index}`].play();
+   });
+})
+
